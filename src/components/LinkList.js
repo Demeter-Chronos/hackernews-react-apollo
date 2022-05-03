bc5084544f0b89676a2a5f3ae658a22fd03697bd
Demeter Chronos <demeterchronos16604@gmail.com>
2022-05-03 22:29:12 +0100

    More Mutations and Updating the Store

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -11,27 +11,38 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
 `
-;
+  ;
 
 
-  const LinkList = () => {
-    const { data } = useQuery(FEED_QUERY);
-  
-    return (
-      <div>
-        {data && (
-          <>
-            {data.feed.links.map((link) => (
-              <Link key={link.id} link={link} />
-            ))}
-          </>
-        )}
-      </div>
-    );
-  };
+const LinkList = () => {
+  const { data } = useQuery(FEED_QUERY);
+
+  return (
+    <div>
+      {data && (
+        <>
+          {data.feed.links.map((link, index) => (
+            <Link key={link.id} link={link} index={index} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
 
 export default LinkList;
+export {FEED_QUERY};
